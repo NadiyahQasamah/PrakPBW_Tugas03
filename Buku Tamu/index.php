86a5092cfd0320aa93d32882dc2569a64448ea95
NadiyahQasamah <145907307+NadiyahQasamah@users.noreply.github.com>
2024-09-20 09:34:33 +0700

    Add files via upload

---
+++ Buku Tamu/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page - Buku Tamu</title>
+</head>
+
+<body>
+    <h1>Selamat Datang di Buku Tamu</h1>
+    <p>Selamat datang di situs buku tamu kamu. Silahkan mengisi buku tamu
+        atau melihat dafatr tamu yang telah mengisi</p>
+
+    <ul>
+        <li><a href="input.php">Isi Buku Tamu</a></li>
+        <li><a href="table.php">Lihat Daftar Tamu</a></li>
+    </ul>
+    
+</body>
+</html>
